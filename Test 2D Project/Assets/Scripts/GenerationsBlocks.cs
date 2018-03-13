921e2fe02624d1f6c31cd1c75c206d6eb85faf9e
MichailKlyushin <klyushin.mixail@gmail.com>
2018-03-13 21:59:09 +0500

    Added loading panel

---
+++ Test 2D Project/Assets/Scripts/GenerationsBlocks.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerationsBlocks : MonoBehaviour
+{
+
+    private bool use = false;
+
+    public GameObject Block;
+
+    void Start()
+    {
+        transform.parent.gameObject.name = transform.parent.gameObject.name.Replace("(Clone)", "");
+    }
+
+    private void OnTriggerEnter()
+    {
+        if (!use)
+        {
+            Explosion(new Vector2(transform.position.x - 5, transform.position.y), 1.0f);
+            Explosion(new Vector2(transform.position.x + 5, transform.position.y), 1.0f);
+        }
+    }
+
+    void Explosion (Vector2 center, float radius)
+    {
+        Collider[] hitCollider = Physics.OverlapSphere(center, radius);
+        if (hitCollider.Length == 0)
+            Instantiate(Block, center, Quaternion.identity);
+    }
+}
---
+++ Test 2D Project/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  //Бибилиотека для взаимодействия со сценами
+
+public class MainMenuScript : MonoBehaviour {
+
+    //Кнопка "Начать игру"
+    public void OnClickStart()
+    {        
+        SceneManager.LoadScene(1);
+    }
+
+    //Кнопка "Выход"
+    public void OnClickExit()
+    {
+        Application.Quit();
+    }
+}
